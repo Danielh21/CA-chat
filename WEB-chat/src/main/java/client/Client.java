410da53850ed1993be42544fed62485ccbf9b668
Waqas <cph-wm66@cphbusiness.dk>
2016-09-05 12:52:12 +0200

    Added ClientHandler.java

--- WEB-chat/src/main/java/client/Client.java
+++ WEB-chat/src/main/java/client/Client.java
@@ -3,14 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package server;
+package client;
 
 /**
  *
  * @author rasmu
  */
-public class dummy {
-    public static void main(String[] args) {
-        
-    }
+public class Client {
+    
 }
