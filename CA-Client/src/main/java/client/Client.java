c459c9f92d0f04a7bf9e94fbe04cd97419d9eb61
cherr <cherr@10.50.137.236>
2016-09-06 12:26:40 +0200

    Client and GUI

--- CA-Client/src/main/java/client/Client.java
+++ CA-Client/src/main/java/client/Client.java
@@ -1,14 +1,72 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 
  */
 package client;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Observable;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
- * @author rasmu
+ * @author Cherry
  */
-public class Client {
+public class Client extends Observable{
+    Socket socket;
+    private int port;
+    private InetAddress serverAddress;
+    private Scanner input;
+    private PrintWriter output;
+    private Boolean stop;
+    private ClientGUI gui;
+    private String ip;
+    private String message;
     
+    Client(ClientGUI gui){
+        this.gui = gui;
+        //get port and ip from the gui
+        connect(ip,port);
+    }
+
+    private void connect(String ip, int port) {
+        try{
+        this.port = port;
+        serverAddress = InetAddress.getByName(ip);
+        socket = new Socket(serverAddress, port);
+        input = new Scanner(socket.getInputStream());
+        output = new PrintWriter(socket.getOutputStream(), true);  //Set to true, to get auto flush behaviour
+        stop = false;
+        }catch(Exception ex){
+            System.out.println("ERROR:" + ex);
+        }
+    }
+    
+     
+     public void send(String msg){
+         output.println(msg);
+     }
+     
+     public String receive() {
+        message = input.nextLine(); // Blocking call
+        if (message.equals("LOGOUT")) {
+            try {
+                socket.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);// We should have a logger
+            }
+        }
+
+        setChanged();
+        notifyObservers(message);
+        return message;
+    }
+     
+     public boolean isStopped(){
+         return stop;
+     }
 }
