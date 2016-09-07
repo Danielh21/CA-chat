20d7d82da4b94c6d62e1dd4bb1c3a137061484ce
Waqas <cph-wm66@cphbusiness.dk>
2016-09-07 08:52:43 +0200

    Added Decoder.java (incomplete)

---
+++ CA-chat/src/main/java/server/Decoder.java
@@ -0,0 +1,49 @@
+package server;
+
+import java.util.ArrayList;
+
+public class Decoder {
+
+    private String cmd;
+    private ArrayList connectedUsers;
+
+    public Decoder(String input) {
+
+        String[] splitString = input.split(":");
+
+        if (splitString.length > 1) {
+
+            switch (splitString[0]) {
+                case "LOGIN":
+                    login();
+                    break;
+                case "MSG":
+                    sendMsg();
+                    break;
+                case "LOGOUT":
+                    logout();
+                    break;
+                default:
+                    cmd = "SYNTAX ERROR";
+
+            }
+
+        }
+
+    }
+
+    
+
+    private void login() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void sendMsg() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void logout() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
