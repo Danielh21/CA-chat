d328b4cdb800480f0e9ca708b1638a5f03f47f81
cherr <cherr@10.50.137.236>
2016-09-07 13:36:29 +0200

    Latest version for demo

--- CA-chat/src/main/java/server/Decoder.java
+++ CA-chat/src/main/java/server/Decoder.java
@@ -10,7 +10,7 @@ public class Decoder {
     private String[] splitString;
     private String currentUser;
 
-    String result;
+    String result = "Unrecognized command!";
     ArrayList<String> recipientsList;
 
     public Decoder() {
@@ -20,75 +20,48 @@ public class Decoder {
     public void splitLine(String line) {
         String[] words = line.split(":");
         String cmd = words[0];
-
-        switch (cmd.toUpperCase()) {
-
-            case "MSG":
-                ArrayList<String> recipients = new ArrayList<>();
-                String recipient = words[1];
-                String msg = words[2];
-                String[] list = recipient.split(",");
-                //add the list of recipients who will receive the message
-                if (list.length >= 1) {
-
-                    for (int i = 0; i < list.length; i++) {
-                        recipients.add(list[i].toUpperCase());
+        try {
+            
+            switch (cmd.toUpperCase()) {
+                
+                case "MSG":
+                    ArrayList<String> recipients = new ArrayList<>();
+                    String recipient = words[1];
+                    String msg = words[2];
+                    String[] list = recipient.split(",");
+                    //add the list of recipients who will receive the message
+                    if (!recipient.equals("") && list.length >= 1) {
+                        recipients.add(currentUser);
+                        for (int i = 0; i < list.length; i++) {
+                            recipients.add(list[i].toUpperCase());
+                        }
                     }
-                }
-                String response = generateResponse(recipients, msg);
-                if (recipients.isEmpty()) {
-                    ChatServer.sendToAllClients(msg);
-                } else {
-                    ChatServer.sendToSomeClients(recipients, response);
-                }
-                break;
-
-            case "LOGOUT":
-                //check current user and close the connection
-                //send all the clients an update who loggedout
-                break;
-
-            default:
-                result = "Unrecognized command!";
-                break;
+                    String response = generateResponse(recipients, msg);
+                    if (recipients.isEmpty()) {
+                        ChatServer.sendToAllClients(response);
+                    } else {
+                        ChatServer.sendToSomeClients(recipients, response);
+                    }
+                    break;
+                
+                default:
+                    break;
+            }
+        } catch (Exception e) {
+            ArrayList<String> rec = new ArrayList<>();
+            rec.add(currentUser.toUpperCase());
+            ChatServer.sendToSomeClients(rec, result);
         }
     }
 
-//    public String getCmd() {
-//        return cmd;
-//    }
-//    private void login() {
-//        connectedUsers.add(splitString[1]);
-//        for (Object connectedUser : connectedUsers) {
-//            System.out.println(connectedUser.toString());
-//        }
-//    }
     public String generateResponse(ArrayList<String> recipients, String msg) {
         String mesRes = "";
-        //check if the recipients is null, if null.. it needs to send to all
-        //holding the recipients list in the recipients arrayList
-//        for (String recipient : recipients) {
-//            recipientsList.add(recipient);
-//        }
         mesRes = "MSGRES:" + currentUser + ":" + msg;
         System.out.println(mesRes);
         return mesRes;
     }
 
-    private void logout() {
 
-    }
-
-    public ArrayList<String> getRecipients() {
-        return recipientsList;
-    }
-
-//    public static void main(String[] args) {
-//        Decoder d = new Decoder();
-//        String line = "MSG::This is just a trial!";
-//        d.splitLine(line);
-//        System.out.println("Recipients:" + d.getRecipients());
-//    }
     public void setCurrentUserName(String username) {
         currentUser = username;
     }
